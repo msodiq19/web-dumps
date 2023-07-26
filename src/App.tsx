2bb027e66002efba0293d55a943120455fe3e176
Muhammed Sodiq <muhammedsodiq2018@gmail.com>
2023-07-26 20:55:23 +0100

    app componet

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import Hero from './components/Hero_components/Hero'
+import Navabar from './components/Navigation_Bars/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <Navabar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
